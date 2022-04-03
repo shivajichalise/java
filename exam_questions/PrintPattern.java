35c3c9a8bc1f04877075b523842489411223f60d
Shivaji Chalise <respond2shivaji@gmail.com>
2022-04-03 12:03:08 +0545

    Added another solution

---
+++ exam_questions/PrintPattern.java
@@ -0,0 +1,10 @@
+class PrintPattern {
+  public static void main(String[] a){
+    for(int i=0;i<5; i++){
+      for(int j=0; j<=i; j++){
+          System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+}
